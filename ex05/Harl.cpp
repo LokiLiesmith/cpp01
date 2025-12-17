d14b82a1ef7c4cda498e743eb7bc381f3f70cba2
Mark Razem <mrazem@3-D-10.42heilbronn.de>
2025-12-17 22:33:16 +0100

    cleanup Harl

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: mrazem <mrazem@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 22:13:32 by mrazem            #+#    #+#             */
-/*   Updated: 2025/12/17 22:13:33 by mrazem           ###   ########.fr       */
+/*   Updated: 2025/12/17 22:32:47 by mrazem           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,30 @@ Harl::~Harl()
 
 void Harl::debug(void)
 {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
+	std::cout << "[DEBUG]\n"
+				<< "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger.\n"
+				<< "I really do!"
 				<< std::endl;
 }
 void Harl::info(void)
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!"
+	std::cout << "[INFO]\n"
+				<< "I cannot believe adding extra bacon costs more money.\n"
+				<< "You didn't put enough bacon in my burger!\n"
+				<< "If you did, I wouldn't be asking for more!"
 				<< std::endl;
 }
 void Harl::warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month." 
+	std::cout << "[WARNING]\n"
+				<< "I think I deserve to have some extra bacon for free.\n"
+				<< "I've been coming for years, whereas you started working here just last month." 
 	 			<< std::endl;
 }
 void Harl::error(void)
 {
-	std::cout << "This is unacceptable! I want to speak to the manager now." 
+	std::cout << "[ERROR]\n"
+				<< "This is unacceptable! I want to speak to the manager now." 
 				<< std::endl;
 }
 
