7ed936e58bd16b2f3f3f39b1d9a1a7bd3fa3d3ef
Mark Razem <mrazem@3-D-10.42heilbronn.de>
2025-12-17 22:18:17 +0100

    Harl's whining - array of pointes to member funcs that return void

---
+++ ex05/Harl.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrazem <mrazem@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/17 22:13:26 by mrazem            #+#    #+#             */
+/*   Updated: 2025/12/17 22:13:26 by mrazem           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <string>
+#include <iostream>
+
+class Harl
+{
+	private:
+
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+
+	public:
+		Harl();
+		~Harl();
+		
+		void	complain(std::string level);
+};
+
+#endif
---
+++ ex05/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrazem <mrazem@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/17 22:13:22 by mrazem            #+#    #+#             */
+/*   Updated: 2025/12/17 22:16:14 by mrazem           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+#include <iostream>
+#include <string>
+
+int main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		std::cerr << "Usage: ./harl <LEVEL>\nLevels: DEBUG, INFO, WARNING, ERROR" << std::endl;
+		return (1);
+	}
+
+	Harl harl;
+	std::string level = av[1];
+	harl.complain(level);
+	return (0);
+}
+
+
